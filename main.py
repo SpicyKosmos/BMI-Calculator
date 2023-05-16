7bd1f457f5ef38d8be00ba44eb5252eafb5b5f41
Kaan Zati <kaan.zati@gmail.com>
2023-05-17 02:33:41 +0300

    edit

--- main.py
+++ main.py
@@ -3,23 +3,23 @@ from tkinter import *
 
 canvas = Tk()
 canvas.title("BMI Calculator")
-canvas.minsize(300,400)
+canvas.minsize(300, 400)
 
-my_font = ("Arial",12,"bold")
-
-
-weight_label = Label(text="Enter your weight (kg)",font=my_font)
-weight_label.place(x=150-87,y=80)
+my_font = ("Arial", 12, "bold")
 
 
-kg_text= Entry()
-kg_text.place(x=150-62,y=110)
+weight_label = Label(text="Enter your weight (kg)", font=my_font)
+weight_label.place(x=150-87, y=80)
 
-height_label = Label(text="Enter your height (cm)",font=my_font)
-height_label.place(x=150-87,y=170)
 
-cm_text= Entry()
-cm_text.place(x=150-62,y=200)
+kg_text = Entry()
+kg_text.place(x=150-62, y=110)
+
+height_label = Label(text="Enter your height (cm)", font=my_font)
+height_label.place(x=150-87, y=170)
+
+cm_text = Entry()
+cm_text.place(x=150-62, y=200)
 
 
 result_msg = None
@@ -27,7 +27,7 @@ error_msg = None
 
 
 def bmi_calculate():
-    global result_msg,error_msg
+    global result_msg, error_msg
 
     if result_msg is not None:
         result_msg.destroy()
@@ -41,16 +41,16 @@ def bmi_calculate():
         kilo = int(kg_text.get())
         cm = int(cm_text.get())
         try:
-            cm/=100
-            cm*=cm
-            result=kilo/cm
-            result_final=round(result,2)
+            cm /= 100
+            cm *= cm
+            result = kilo/cm
+            result_final = round(result, 2)
 
-            if result_final<18.5:
+            if result_final < 18.5:
 
                 result_msg = Label(text=f"Your BMI is {result_final}\nYour weight type: Under Weight ", font=my_font, fg="red")
                 result_msg.place(x=25, y=300)
-            elif 18.5 <= result_final <=24.9:
+            elif 18.5 <= result_final <= 24.9:
 
                 result_msg = Label(text=f"Your BMI is {result_final}\nYour weight type: Normal ", font=my_font, fg="red")
                 result_msg.place(x=50, y=300)
@@ -67,7 +67,7 @@ def bmi_calculate():
 
             elif 35 <= result_final <= 39.9:
 
-                result_msg = Label(text=f"Your BMI is {result_final}\nYour weight type: Obesity (Class II) ", font=my_font,fg="red")
+                result_msg = Label(text=f"Your BMI is {result_final}\nYour weight type: Obesity (Class II) ", font=my_font, fg="red")
                 result_msg.place(x=15, y=300)
 
             if result_final >= 40:
@@ -87,8 +87,8 @@ def bmi_calculate():
 
 
 
-calculator_button= Button(text="Calculate",height=1,width=10,command=bmi_calculate)
-calculator_button.place(x=150-40,y=260)
+calculator_button = Button(text="Calculate",height=1, width=10, command=bmi_calculate)
+calculator_button.place(x=150-40, y=260)
 
 
 
